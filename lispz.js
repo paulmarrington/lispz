c633dd67840521657a8817e292381bf93ec81115
paulmarrington <paul@marrington.net>
2015-07-27 19:22:16 +1000

    update comments and implement requires and export

--- lispz.js
+++ lispz.js
@@ -1,9 +1,9 @@
 var lispz = function() {
     // characters that are not space separated atoms (n becomes linefeed in regex)
-    var delims = "(){}[]n".split('');
+    var delims = "(){}[]".split('');
     var not_delims = delims.join("\\");
     delims = delims.join('|\\')
-    var stringRE = "'{1,2}[\\s\\S]*?'{1,2}|" + '"[\\s\\S]*?[^\\\\]"|""|';
+    var stringRE = "'{1,2}[\\s\\S]*?'{1,2}|" + '"[\\s\\S]*?[^\\\\]"|""|' + "###+.*?###+|";
     // Keep state stack for when we start a new inner list ()[]{}
     var push_state = function(env) {
         env.stack.push(env.list);
@@ -32,19 +32,25 @@ var lispz = function() {
       var dict = [], kvp = list[1];
       for (var i = 1, l = kvp.length; i < l; i++) {
         var key = (kvp[i][0] == ".atom") ? kvp[i][1] : list2js(env, kvp[i]);
-        if (key.slice(-1) === ":") {
-          dict.push(key.slice(0, -1)+":"+list2js(env, kvp[++i]));
+        if (key.slice(-1)[0] === ":") {
+          dict.push(key.slice(0, -1)[0]+":"+list2js(env, kvp[++i]));
         } else {
           dict.push(key+":"+key);
         }
       }
       return "{" + dict.join(',') + "}";
     }
+    // Special to set variables loaded with requires
+    var dot_requires = function(env, list) {
+      for (var js = [], i = 1, l = list[1].length; i < l; i++) {
+        var name = list[1][i][1].slice(1,-1).split('/').pop();
+        js.push(name+'=lispz.cache["'+name+'"]');
+      }
+      return "\nvar "+js.join(',')+";\n";
+    }
     // Convert a list of lists to a list of js fragments
     var lists2list = function(env, lists) {
-      return lists.map(function(list, index, parent) {
-        env.parent = parent; env.parent_index = index;
-        return list2js(env, list);
+      return lists.map(function(list, index) { return list2js(env, list);
       }).filter(function(a) { return a && a.length });
     };
     // convert a list to a javascript call
@@ -53,8 +59,11 @@ var lispz = function() {
       if (env.list2js[list[1][1]]) {
         return env.list2js[list[1][1]](env, list.slice(1))
       }
-      var params = lists2list(env, list.slice(2))
-      return " __at__=" + list2js(env, list[1]) + "(" + params.join(',') + ')\n'
+      var params = lists2list(env, list.slice(2)), func = list2js(env, list[1]);
+      if (func.length)
+        return func + "(" + params.join(',') + ')\n'
+      else // caused by the likes of .js
+        return params.join('\n');
     };
     // Convert (js parameters) to javascript
     params2js = function(env, list) {
@@ -82,11 +91,12 @@ var lispz = function() {
       return '"'+lines.join('\\n')+'"';
     };
     // Account for "strings" and "symbols" and convert rest to something js-y
-    var jsify = function(atom) {
-      var last = atom.length - 1;
-      if (atom[0] === '"' && atom[last] === '"') return multiline(atom.slice(1,last));
-      if (atom[0] === "'" && atom[last] === "'") return atom.slice(1,last);
-      return atom.replace(/\W/g, function(symbol) {
+    var jsify = function(a) {
+      var last = a.length - 1;
+      if (a.length > 6 && a.slice(0,3) === "###" && a.slice(-3) === "###") return "";
+      if (a[0] === '"' && a[last] === '"') return multiline(a.slice(1,last));
+      if (a[0] === "'" && a[last] === "'") return a.slice(1,last);
+      return a.replace(/\W/g, function(symbol) {
           var rep = replacements[symbol];
           return rep ? rep : symbol
       })
@@ -97,8 +107,8 @@ var lispz = function() {
     };
     // convert a list to a function definition
     var lambda2js = function(env, list) {
-      return "(function(" + list2params(list[1]).join(',') + "){" +
-          lists2list(env, list.slice(2)).join(" ") + "})"
+      return "function(" + list2params(list[1]).join(',') + "){" +
+          lists2list(env, list.slice(2)).join(" ") + "}"
     };
     // retrieve the next atom from the input stream. Returns false on no more
     var next_atom = function(env) {
@@ -111,24 +121,22 @@ var lispz = function() {
       if (env.delimiter[env.atom]) {
         env.delimiter[env.atom](env)
       } else {
-        if (env.atom[0] == '@') env.atom = "__at__." + env.atom.slice(1);
         env.list.push([".atom", env.atom])
       }
     };
     // Create a lambda that expands a named macro
-    var build_macro = function(env, list, async) {
+    var build_macro = function(env, list) {
       var macro_name = list2js(env, list[1]);
-      var macro_params = {".callback":list[2].length}, body = list.slice(3);
+      var macro_params = {}, body = list.slice(3);
       list2params(list[2]).forEach(function(p, i) { macro_params[p] = i + 1 })
+
       var clone = function(macro_body, replacements) {
         if (macro_body[0] === ".atom") {
-          var is_ref = (macro_body[1][0] === '*'); // *rest
-          var name = is_ref ? macro_body[1].slice(1) : macro_body[1];
+          var atom = macro_body[1].split('*'), name = atom.slice(-1)[0];
           if (macro_params[name]) { // replace with param or list of rest
-            if (name === ".callback") async = true;
-            if (!is_ref) return replacements[macro_params[name]];
+            if (atom.length === 1) return replacements[macro_params[name]];
             macro_body = replacements.slice(macro_params[name]);
-            macro_body.unshift("[");
+            macro_body.unshift((atom.length === 2) ? "[" : ".list");
             return macro_body;
           }
         }
@@ -137,18 +145,8 @@ var lispz = function() {
           return element;
         })
       }
-      if (async) {
-        env.list2js[macro_name] = function(env, replacements) {
-          callback = ["lambda", ["(",[".atom","__at__"]]];
-          callback = callback.concat(env.parent.slice(++env.parent_index));
-          env.parent.length = env.parent_index;
-          replacements = replacements.concat([callback]);
-          return lists2list(env, clone(body, replacements)).join('');
-        }
-      } else {
-        env.list2js[macro_name] = function(env, replacements) {
-          return lists2list(env, clone(body, replacements)).join('');
-        }
+      env.list2js[macro_name] = function(env, replacements) {
+        return lists2list(env, clone(body, replacements)).join('');
       }
       return '';
     };
@@ -156,11 +154,6 @@ var lispz = function() {
     var alias = function(env, list) {
         for (var i = 1, l = list.length; i < l; i += 2)
           env.alias[list[i][1]] = list[i + 1][1]
-    };
-    // put raw javascript at end of last atom
-    var append_raw = function(env, raw) {
-      var list = env.list.length ? env.list : env.stack[env.stack.length - 1];
-      list.push([".raw", raw]);
     };
     // Environment under which a lispz command executes
     var env = {
@@ -170,20 +163,19 @@ var lispz = function() {
         // Called for delimiters when they are found in the input stream
         delimiter: { // start gathering a list or performing other delimiter function
             '(': push_state, '[': push_state, '{': push_state,
-            ')': pop_state, ']': pop_state, '}': pop_state,
-            '\n': function(env) { append_raw(env, " //#"+(env.line_number++)+"\n") }
+            ')': pop_state, ']': pop_state, '}': pop_state
         },
         // called for closing delimiters when encountered in the stream
         list2js: { // process a list once gathered.
             '(': call2js,
             '[': function(env, list) { // list of atoms (array)
-                   return '[' + lists2list(env, list.slice(1)).join(',') + ']'
-                 },
+                   return '[' + lists2list(env, list.slice(1)).join(',') + ']' },
+            '.list': function(env, list) {
+                       return lists2list(env, list.slice(1)).join(' '); },
             'lambda': lambda2js, 'macro': build_macro,
-            'async': function(env, list) { return build_macro(env, list, true) },
             '.atom': function(env, list) { return jsify(list[1]) },
-            '.raw': function(env, list) { return list[1] },
-            '.js': params2js, ".pairs": dot_pairs, ".dict": dot_dict, 'alias': alias
+            '.js': params2js, ".pairs": dot_pairs, ".dict": dot_dict, 'alias': alias,
+            '.requires': dot_requires
         },
         alias: {}
     };
@@ -205,28 +197,38 @@ var lispz = function() {
       return js;
     };
     // To run a lispz statement, wrap it in a function
-    var run = function(script) {
-      var js = "var __at__;\n"+compile(script);
-      return (new Function(js))();
-    }
-    // lispz script loader (needed now to get core.lispz)
-    var load_cache = {};
-    load = function(uri, callback) {
-      if (!callback) callback = function(){return {}};
-      if (load_cache[uri] !== undefined) return callback(load_cache[uri]);
+    var run = function(script) { return (new Function(compile(script)))(); }
 
-      var req = new XMLHttpRequest();
+    var cache = {}
+
+    var load_one = function(uri, callback) {
+      if (cache[uri] !== undefined) return callback(cache[uri]);
+      var req = new XMLHttpRequest(), done = function(response) {
+        // use require name on future calls - watch for name clashes.
+        callback(cache[uri.split('/').pop()] = cache[uri] = response);
+      };
       req.open("GET", uri+".lispz", true);
-      req.onerror = function (err) {
-        callback("The script " + uri + " is not accessible.");
-      }
+      req.onerror = function (err) { done({uri:uri,error:err}); }
       req.onload = function() {
-        if (req.status != 200) return req.error(req.statusText);
-        var js = "var __at__;\n"+compile(req.responseText);
-        callback(null, load_cache[uri] = run(req.responseText));
+        if (req.status != 200) return req.onerror(req.statusText);
+        done(run(req.responseText));
       }
       req.send();
     };
-    window.onload = function(){ load('core') }
-    return { compile:compile, env:env, load:load, run:run }
+    var load = function(uri_list, callback) {
+      if (!callback) callback = function(){return {}};
+      var outstanding = uri_list.length, uris = uri_list.slice();
+      var next_uri = function() {
+        if (uris.length) load_one(uris.shift(), next_uri);
+        if (!outstanding--) callback();
+      }; next_uri();
+    }
+
+    if (window) window.onload = function(){
+      load(['core']);
+      var scripts = window.document.getElementsByTagName('script');
+      for (var i = 0, l = scripts.length; i < l; i++)
+        if (scripts[i].type == "text/lispz") run(scripts[i].innerHTML);
+    };
+    return { compile:compile, env:env, load:load, run:run, cache:cache }
 }()
