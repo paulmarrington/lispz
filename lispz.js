8ed2d3bbaff730508aedf573fb039705c8a2b1f8
Paul Marrington <paul@marrington.net>
2015-08-08 12:07:11 +1000

    using, lists, defined?

--- lispz.js
+++ lispz.js
@@ -5,18 +5,18 @@ var lispz = function() {
   tkre = new RegExp('(' + stringRE + '\\' + delims + "|[^\\s" + not_delims + "]+)", 'g'),
   opens = new Set("({["), closes = new Set(")}]"), ast_to_js, slice = [].slice,
   jsify = function(atom) {
-    if (/^'.*'$/.test(atom)) return atom.slice(1, -1)
+    if (/^'.*'$/.test(atom)) return atom.slice(1, -1).replace(/\\n/g, '\n')
     if (/^"(?:.|\r*\n)*"$/.test(atom)) return atom.replace(/\r*\n/g, '\\n')
     switch (atom[0]) {
-      case '.': return (atom.length > 1) ? "$$"+atom : "$$"
+      case '.': return (atom.length > 1) ? "__"+atom : "__"
       case '@': return (atom.length > 1) ? "this."+atom.slice(1) : "this"
       default:  return atom.replace(/\W/g, function(c) {
-        var t = "bhpalmcewqgutkri"["!#%&+-:;<=>?@\\^~".indexOf(c)]; return t ? ("$"+t+"$") : c })
+        var t = "$hpal_cewqgutkri"["!#%&+-:;<=>?@\\^~".indexOf(c)]; return t ? ("_"+t+"_") : c })
     }
   },
   call_to_js = function(params, body) {
     return (macros[params]) ? macros[params].apply(lispz, slice.call(arguments, 1)) :
-      '$$=' + ast_to_js(params) + '(' + slice.call(arguments, 1).map(ast_to_js).join(',') + ')'
+      '__=' + ast_to_js(params) + '(' + slice.call(arguments, 1).map(ast_to_js).join(',') + ')'
   },
   macro_to_js = function(name, pnames, body) {
     body = slice.call(arguments, 2)
@@ -24,28 +24,31 @@ var lispz = function() {
       pvalues = slice.call(arguments)
       var args = {}
       pnames.slice(1).forEach(function(pname, i) {
-        args[pname] = (pname[0] === '*') ? ["["].concat(pvalues.slice(i)) : pvalues[i]
+        args[pname] = (pname[0] === '*') ? ["list"].concat(pvalues.slice(i)) : pvalues[i]
       })
       var expand = function(ast) {
         return (ast instanceof Array) ? ast.map(expand) : args[ast] || ast
       }
-      return ast_to_js(expand((body.length > 1) ? ["["].concat(body) : body[0]))
+      return ast_to_js(expand((body.length > 1) ? ["list"].concat(body) : body[0]))
     }
     return '""'
   },
   array_to_js = function(its) {
-    its = slice.call(arguments)
-    var js = its.map(ast_to_js).join(',')
-    return (its.length === 1 && its[0][0] === '[') ? "[" + js + "]" : js
+    var js_array = (arguments.length === 1 && arguments[0][0] === '[')
+    its = slice.call(arguments).map(ast_to_js).join(',')
+    return js_array ? "[" + its + "]" : its
+  },
+  list_to_js = function(its) {
+    return slice.call(arguments).map(ast_to_js).join('\n')
   },
   // A dictionary can be a symbol table or k-value pair
   dict_to_js = function(kvp) {
     var dict = []; kvp = slice.call(arguments)
     for (var key, i = 0, l = kvp.length; i < l; i++) {
       if ((key = kvp[i]).slice(-1)[0] === ":") {
-        dict.push(key.slice(0, -1)[0]+":"+ast_to_js(kvp[++i]));
+        dict.push("'"+key.slice(0, -1)[0]+"':"+ast_to_js(kvp[++i]));
       } else {
-        dict.push(key+":"+key);
+        dict.push("'"+key+"':"+ast_to_js(key));
       }
     }
     return "{" + dict.join(',') + "}";
@@ -88,34 +91,34 @@ var lispz = function() {
   },
   ast_to_js = function(ast) {
     return (ast instanceof Array) ? macros[ast[0]] ?
-      macros[ast[0]].apply(this, ast.slice(1)) : array_to_js.apply(null, ast) : jsify(ast)
+      macros[ast[0]].apply(this, ast.slice(1)) : list_to_js(ast) : jsify(ast)
   },
   compile = function(source) {
-    return parse_to_ast(source).map(ast_to_js).join(';\n')
+    return parse_to_ast(source).map(ast_to_js).join('\n')
   },
   run = function(source) {
     return parse_to_ast(source).map(ast_to_js).map(eval)
   },
   //######################### Script Loader ####################################//
+  http_request = function(uri, type, callback) {
+    var req = new XMLHttpRequest()
+    req.open(type, uri, true)
+    req.onerror = function(err) {
+      callback({ uri: uri, error: err })
+    }
+    req.onload = function() {
+      if (req.status === 200) callback({ uri:uri, text: req.responseText })
+      else                    req.onerror(req.statusText) 
+    }
+    req.send()
+  },
   cache = {},
   load_one = function(uri, callback) {
     if (cache[uri] !== undefined) return callback(cache[uri])
-    var req = new XMLHttpRequest(),
-      done = function(response) {
-        callback(cache[uri.split('/').pop()] = cache[uri] = response)
-      }
-    req.open("GET", uri + ".lispz", true)
-    req.onerror = function(err) {
-      done({
-        uri: uri,
-        error: err
-      })
-    }
-    req.onload = function() {
-      return (req.status != 200) ? req.onerror(req.statusText) : done(
-        (new Function(compile(req.responseText)))())
-    }
-    req.send()
+    http_request(uri + ".lispz", 'GET', function(response) {
+      response = response.text ? (new Function(compile(response.text))()) : null;
+      callback(cache[uri.split('/').pop()] = cache[uri] = response)
+    })
   },
   load = function(uri_list, callback) {
     var uris = uri_list.split(','), outstanding = uris.length
@@ -130,18 +133,19 @@ var lispz = function() {
     return 'var ' + list.slice(1,-1).split(',').map(function(module) {
       var name = module.trim().split('/').pop()
       return name + '=lispz.cache["' + name + '"]'
-    }) + '\n'
+    }) + ';'
   }
   if (window) window.onload = function() {
     load('core', function() {})
   }
   //######################### Script Loader ####################################//
   var macros = {
-    '(': call_to_js, '[': array_to_js, '{': dict_to_js, macro: macro_to_js, join: join_to_js,
-    '#pairs': pairs_to_js, '#binop': binop_to_js, '#requires': requires_to_js
+    '(': call_to_js, '[': array_to_js, '{': dict_to_js, 'macro': macro_to_js, 'join': join_to_js,
+    '#pairs': pairs_to_js, '#binop': binop_to_js, '#requires': requires_to_js, 'list': list_to_js
   }
   // add all standard binary operations (+, -, etc)
-  "+,-,*,/,&&,||,==,===,<=,>=,!=,<,>,^".split(',').forEach(binop_to_js)
+  "+,-,*,/,&&,||,==,===,<=,>=,!=,!==,<,>,^".split(',').forEach(binop_to_js)
 
-  return { compile: compile, run: run, parsers: parsers, load: load, macros: macros, cache: cache }
+  return { compile: compile, run: run, parsers: parsers, load: load, macros: macros, cache: cache,
+           http_request: http_request}
 }()
