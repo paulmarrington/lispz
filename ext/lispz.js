5db4a73fba4583d6dbf39dcc6cb55c1e5f5eec03
Paul Marrington <paul@marrington.net>
2015-11-06 13:52:48 +1100

    lispz release code

--- ext/lispz.js
+++ ext/lispz.js
@@ -1,1 +1,15 @@
-window.lispz_modules={}
+window.lispz_modules={}function (){var _h_callbacks=[];//#core:72
+
+var _h_pledge=(new Promise((function(ok,fail){_h_callbacks={'ok':ok,'fail':fail};})));//#core:73
+
+var resolve_promise=(function(){_h_callbacks.ok.apply(null,[].slice.call(arguments,0))});//#core:74
+
+var reject_promise=(function(err){_h_callbacks.fail(err)});//#core:75
+
+lispz.globals.promise.all(names.map(load_riot)).then((function(){return resolve_promise(source)
+//#dev:47
+}))//#dev:48
+
+return _h_pledge
+//#core:78
+}
